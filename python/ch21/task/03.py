5329c61b4dbd3b59cb4697fbc5b0835e790a5242
KDG0409 <korvic6310@gmail.com>
2025-10-14 12:09:54 +0900

    modified

--- python/ch21/task/03.py
+++ python/ch21/task/03.py
@@ -7,16 +7,18 @@ image = cv2.imread("C:/rokey/python/ch21/task/people.jpg")
 face_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_path)
 
+resized = cv2.resize(image,(300,300))
+
 # 이미지 흑백 전환
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 # 얼굴 검출 수행
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30))
 
 # 검출된 얼굴에 사각형 그리기
 for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.rectangle(resized,(x,y),(x+w,y+h),(255,0,0),2)
 
 # 결과 출력
-cv2.imshow('Face Detection',image)
+cv2.imshow('Face Detection',resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
