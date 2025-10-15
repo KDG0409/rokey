63f82a75c40594bab87f8dfefda6a5f0142c662d
KDG0409 <korvic6310@gmail.com>
2025-10-15 23:15:52 +0900

    modified

---
+++ python/Test/Week/01re.py
@@ -0,0 +1,34 @@
+#1. 인터프린터,객체지향적,클래스지원,다양한 라이브러리 지원
+#2. 
+# a=int(input('첫번째수:'))
+# b=int(input('두번째수:'))
+# print(a+b,a-b,a*b,a/b)
+#3. a:Float, b:Boolean, c:String
+#4. 
+# score=int(input('점수를 입력하세요.:'))
+# if score<=100 and score>=90:
+#     print("A")
+# elif score<90 and score>=80:
+#     print("B")
+# elif score<80 and score>=70:
+#     print("c")
+# elif score<70 and score>=0:
+#     print("F")
+#5.
+# numbers=[1,2,3,4,5]
+# for num in numbers:
+#     print(num,end='')
+#6.
+# age=int(input("나이를 입력하세요.:"))
+# if age>=20 and age<=60:
+#     print("적정입니다.")
+# else:
+#     print("적정아닙니다.")
+#7.
+# for i in [1,5,9,13,9,5,1]:
+#     print("*"*i)
+#8.
+# fruits = ['바나나', '파인애플', '복숭아', '사과', '포도']
+# for fruit in fruits:
+#     if fruit=='사과':
+#         print('사과를 찾았습니다.')
---
+++ python/Test/Week/04re.py
@@ -0,0 +1,23 @@
+#1.
+class Stack:
+    def __init__(self):
+        self.stack=['두산','로키','부트']
+    def push(self,data):
+        return self.stack.append(data)
+ans=Stack()
+print(ans.push('캠프'))
+#2.
+class Stack:
+    def __init__(self):
+        self.stack=['두산','로키','부트']
+    def push(self,data):
+        self.stack.append(data)
+        return self.stack
+    def pop(self):
+        self.stack.pop()
+        return self.stack
+ans=Stack()
+print(ans.push('캠프'))
+print(ans.pop())
+#3.트
+#4.트
